7876321690cc626db7f3644b3057a76d0b4a5baa
ArmandBurger <22417562+ArmandBurger@users.noreply.github.com>
2026-01-11 19:14:46 +0200

    components wip

---
+++ bevyml-components/src/lib.rs
@@ -0,0 +1,62 @@
+use bevy_ecs::component::Component;
+use serde::{Deserialize, Serialize};
+
+pub trait Element: Component {}
+
+macro_rules! element {
+    ($name:ident) => {
+        #[derive(Component, Clone, Copy, Debug, Default, Serialize, Deserialize)]
+        pub struct $name;
+
+        impl Element for $name {}
+    };
+}
+
+element!(HtmlElement);
+element!(HeadElement);
+element!(BodyElement);
+element!(TitleElement);
+element!(MetaElement);
+element!(LinkElement);
+element!(StyleElement);
+element!(ScriptElement);
+element!(DivElement);
+element!(SpanElement);
+element!(PElement);
+element!(AElement);
+element!(ImgElement);
+element!(ButtonElement);
+element!(InputElement);
+element!(LabelElement);
+element!(TextareaElement);
+element!(SelectElement);
+element!(OptionElement);
+element!(UlElement);
+element!(OlElement);
+element!(LiElement);
+element!(TableElement);
+element!(TheadElement);
+element!(TbodyElement);
+element!(TfootElement);
+element!(TrElement);
+element!(ThElement);
+element!(TdElement);
+element!(HeaderElement);
+element!(FooterElement);
+element!(NavElement);
+element!(MainElement);
+element!(SectionElement);
+element!(ArticleElement);
+element!(AsideElement);
+element!(FormElement);
+element!(CanvasElement);
+element!(SvgElement);
+element!(BrElement);
+element!(HrElement);
+
+element!(H1Element);
+element!(H2Element);
+element!(H3Element);
+element!(H4Element);
+element!(H5Element);
+element!(H6Element);
